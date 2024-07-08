abdea0aba140b5b50d83e0ea350d44ae35c87909
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-07-08 05:11:58 +0300

    Add tasks data in file

---
+++ task-list/src/app/mock-tasks.ts
@@ -0,0 +1,19 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    title: 'Doctors Appointment',
+    completed: true,
+  },
+  {
+    id: 2,
+    title: 'Meeting at School',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'Food Shopping',
+    completed: false,
+  },
+];
