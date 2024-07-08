796ec3eef9737f32afa931f390d79d8e2e7215ca
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-07-08 05:07:18 +0300

    Add reusable button component

--- task-list/src/app/components/header/header.component.ts
+++ task-list/src/app/components/header/header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-title: string = 'Task List';
+  title: string = 'Task List';
+
+  addTask() {
+    console.log('add task');
+  }
 }
