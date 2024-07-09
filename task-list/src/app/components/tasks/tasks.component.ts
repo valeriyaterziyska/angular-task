9b7bed80c676a78b7c1a80143a36ce87fd086682
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-07-09 14:59:04 +0300

    Add task service

--- task-list/src/app/components/tasks/tasks.component.ts
+++ task-list/src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {
+
+  }
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
 }
