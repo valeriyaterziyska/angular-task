c2a5655e27b62193559abb2575ba83500a26acea
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-07-15 03:23:53 +0300

    Add ui service for toggle

--- task-list/src/app/services/ui.service.ts
+++ task-list/src/app/services/ui.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable, Subject} from 'rxjs';
+import { TaskService } from './task.service';
+import { Task } from '../Task';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +10,27 @@ export class UiService {
   private showAddTask: boolean = false;
   private subject = new Subject<any>();
    
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   toggleAddTask(): void {
     this.showAddTask = !this.showAddTask;
     this.subject.next(this.showAddTask);
   }
+
+  sortTasks(): void {
+    // console.log("ui service sort");
+    
+    // this.taskService.getTasks().subscribe((tasks) => {
+      
+    //   tasks = tasks.sort((a, b) => (a.title.localeCompare(b.title)));
+    //   console.log('sorted', tasks);
+      
+    // });
+  }
+
+  // getTasks(): Observable<Task[]> {
+  //   return this.http.get<Task[]>(this.apiURL);
+  // }
 
   onToggle(): Observable<any> {
     return this.subject.asObservable();
