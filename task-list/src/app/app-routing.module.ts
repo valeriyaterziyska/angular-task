9a1172bd4167a083e8d4dd5a2c0e8588236ac9fe
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-07-15 03:21:56 +0300

    Update app-routing module

--- task-list/src/app/app-routing.module.ts
+++ task-list/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { EditTaskComponent } from './components/edit-task/edit-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HeaderComponent,
+  },
+  {
+    path: ':id',
+    component: EditTaskComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
